31e9475bdfda442d35bfb818f2ac8afe0bd73d58
Zach <shermanzach@gmail.com>
2021-12-17 23:31:16 -0800

    Follow setup in https://github.com/runelite/plugin-hub README

--- src/main/java/com/ultimatestorage/UltimateStoragePlugin.java
+++ src/main/java/com/ultimatestorage/UltimateStoragePlugin.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.ultimatestorage;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
@@ -14,15 +14,15 @@ import net.runelite.client.plugins.PluginDescriptor;
 
 @Slf4j
 @PluginDescriptor(
-	name = "Example"
+	name = "Ultimate Storage"
 )
-public class ExamplePlugin extends Plugin
+public class UltimateStoragePlugin extends Plugin
 {
 	@Inject
 	private Client client;
 
 	@Inject
-	private ExampleConfig config;
+	private UltimateStorageConfig config;
 
 	@Override
 	protected void startUp() throws Exception
@@ -46,8 +46,8 @@ public class ExamplePlugin extends Plugin
 	}
 
 	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
+	UltimateStorageConfig provideConfig(ConfigManager configManager)
 	{
-		return configManager.getConfig(ExampleConfig.class);
+		return configManager.getConfig(UltimateStorageConfig.class);
 	}
 }
--- src/test/java/com/ultimatestorage/UltimateStoragePluginTest.java
+++ src/test/java/com/ultimatestorage/UltimateStoragePluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.ultimatestorage;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class UltimateStoragePluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(UltimateStoragePlugin.class);
 		RuneLite.main(args);
 	}
 }
